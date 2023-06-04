7b50bea54d697c2dd4f165c90730d243af2e08c4
Andreas Pandu Pamungkas <andspemail@gmail.com>
2023-06-04 14:12:11 +0700

    Update konten kelompok

--- src/Component/KelompokComponent.js
+++ src/Component/KelompokComponent.js
@@ -5,16 +5,16 @@ const KelompokComponent = () =>
     return(
         <div className="container mt-5">
 
-            <div class="menu-back mb-5">
-                <Link class="back-button" to="/">
-                    <i class="fa-solid fa-arrow-left"></i> <span>Kembali</span>
+            <div className="menu-back mb-5">
+                <Link className="back-button" to="/">
+                    <i className="fa-solid fa-arrow-left"></i> <span>Kembali</span>
                 </Link>
             </div>
 
             <div className='menang-kalah'>
                 <div className='content'>
                     <h1>Kelompok 8</h1>
-                    <p>Dibuat pada tanggal 04 Mei 2023. Berikut nama anggota kelompok:</p>
+                    <p>Dibuat pada tanggal 04 Mei 2023 dan selesai pada tanggal 04 Mei 2023. Berikut nama anggota kelompok:</p>
 
                     <div className="mt-4 text-start">
                         <ul>
@@ -26,7 +26,7 @@ const KelompokComponent = () =>
                     </div>
 
                     <div className="mt-5 text-center">
-                        Kecerdasan Buatan / B
+                        Kecerdasan Buatan / B - 2023
                     </div>
                 </div>
             </div>
